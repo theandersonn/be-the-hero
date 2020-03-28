0e38c6e3a44077bbd13fccda4cce7045094ce221
theandersonn <theandersonn@gmail.com>
2020-03-27 21:17:11 -0300

    Aplica validação - backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -7,30 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-/**
- * Rota / Recurso
- */
-
-/**
- * Métodos HTTP:
- * 
- * GET: Buscar/Listar uma informação do back-end
- * POST: Criar uma informação no back-end
- * PUT: Alterar uma informação no back-end
- * DELETE: Deletar uma informação no back-end
- */
-
- /**
-  * Tipos de parâmetros
-  * 
-  * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros e Paginação)
-  * Route Params: Parâmetros utilizados para identificar recursos
-  * Request Body: 
-  */
-
-  /**
-   * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
-   * NoSQL: MongoDB, CouchDB, etc...
-   */
+app.use(errors());
 
 app.listen(3333);
